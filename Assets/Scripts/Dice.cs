21af50e2e74b5d288430bc0c9d548c10f699d11c
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2022-07-15 23:32:41 -0700

    controls

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    public GameObject face;
+    Vector3 lookForward;
+    Vector3 lookUpward;
+    
+    public Vector3Int DirectionValues;
+    private Vector3Int OpposingDirectionValues;
+
+    private Rigidbody rb;
+
+    readonly List<string> FaceRepresent = new() {"", "1", "2", "3", "4", "5", "6"};
+    
+    void Start()
+    {
+        OpposingDirectionValues = 7 * Vector3Int.one - DirectionValues;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (transform.hasChanged)
+        {
+            lookUpward = getDirection(Vector3.up);
+            
+            face.transform.localPosition = 2.0001f * lookUpward;
+            Vector3 rotation = Quaternion.Inverse(transform.rotation).eulerAngles;
+            float x = Mathf.Round(rotation.x/90) * 90;
+            float y = Mathf.Round(rotation.y/90) * 90;
+            float z = Mathf.Round(rotation.z/90) * 90;
+            face.transform.localRotation = Quaternion.Euler(x, y, z);
+
+            transform.hasChanged = false;
+        }
+    }
+    
+    Vector3 getDirection(Vector3 relative)
+    {
+        if (Vector3.Dot(relative, transform.right) > 0.9f)
+        {
+            Debug.Log(relative);
+            return Vector3.right;
+        }
+        else if (Vector3.Dot(relative, -transform.right) > 0.9f)
+        {
+            return Vector3.left;
+        }
+        else if (Vector3.Dot(relative, transform.up) > 0.9f)
+        {
+            return Vector3.up;
+        }
+        else if (Vector3.Dot(relative, -transform.up) > 0.9f)
+        {
+            return Vector3.down;
+        }
+        else if (Vector3.Dot(relative, transform.forward) > 0.9f)
+        {
+            return Vector3.forward;
+        }
+        else if (Vector3.Dot(relative, -transform.forward) > 0.9f)
+        {
+            return Vector3.back;
+        }
+        else
+        {
+            return relative;
+        }
+    }
+
+    void launch(Vector2 direction, float power)
+    {
+        rb.AddForce(new Vector3(direction.x, 5, direction.y) * power, ForceMode.Impulse);
+        rb.AddTorque(Vector3.forward, ForceMode.Impulse);
+    }
+
+    void OnFire()
+    {
+        launch(new Vector2(1,1).normalized, 1);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Range(0f, 30f)]
     [SerializeField] float moveSpeed = 1f;
+    [Range(1f, 100f)] 
+    [SerializeField] private float sensitivity = 1f;
     [SerializeField] Camera cam;
+
+    private Vector3 dir;
 
     // Start is called before the first frame update
     void Start()
@@ -16,28 +22,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-
-        // Debug.Log(ray);
-        Vector3 mousePos = new Vector3();
-        if (Physics.Raycast(ray, out RaycastHit raycastHit))
-        {
-            mousePos = raycastHit.point;
-            // Debug.Log($"Mouse: {mousePos}");
-        }
-        
-        Vector3 lookDirection = new Vector3(mousePos.x - transform.position.x, 0, mousePos.z - transform.position.z);
-        Debug.Log(lookDirection);
-        transform.forward = lookDirection;
+        transform.position += dir * moveSpeed * Time.deltaTime;
+        // Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        //
+        // // Debug.Log(ray);
+        // Vector3 mousePos = new Vector3();
+        // if (Physics.Raycast(ray, out RaycastHit raycastHit))
+        // {
+        //     mousePos = raycastHit.point;
+        //     // Debug.Log($"Mouse: {mousePos}");
+        // }
+        //
+        // Vector3 lookDirection = new Vector3(mousePos.x - transform.position.x, 0, mousePos.z - transform.position.z);
+        // Debug.Log(lookDirection);
+        // transform.forward = lookDirection;
     }
 
-    private void FixedUpdate()
+    public void OnMove(InputValue inputValue)
     {
-        float vert = Input.GetAxisRaw("Vertical");
-        float hori = Input.GetAxisRaw("Horizontal");
-
-        transform.position += new Vector3(hori, 0, vert) * moveSpeed;
-
+        Vector2 inputVector = inputValue.Get<Vector2>();
+        dir = new Vector3(inputVector.x, inputVector.y);
     }
-
+    
+    public void OnLook(InputValue inputValue)
+    {
+        transform.Rotate(Vector3.forward, -inputValue.Get<Vector2>().x * Time.deltaTime * sensitivity);
+    }
 }
