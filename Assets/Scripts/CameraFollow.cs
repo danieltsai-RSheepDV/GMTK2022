b772075d0db1d48a59df45f84ca17b7978b583c6
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2022-07-16 00:42:24 -0700

    shiny cube

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private GameObject target;
+    [SerializeField] private Vector3 offset;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = target.transform.position + offset;
+        transform.LookAt(target.transform);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = target.transform.position + new Vector3(0, 10, -3);
+        transform.position = target.transform.position + offset;
     }
 }
